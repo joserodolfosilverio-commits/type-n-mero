5d8df6ea2d774cf1b88e5198b8c639075a06671f
joserodolfosilverio-commits <jose.rodolfo.silverio@escola.pr.gov.br>
2026-03-05 15:01:18 +0000

    .

--- type-number.js
+++ type-number.js
@@ -4,6 +4,8 @@ const primeiroNumero = 1;
 const segundoNumero = 2;
 const operacaoMatematica = primeiroNumero / segundoNumero;
 
-console.log(operacaoMatematica)
+console.log(operacaoMatematica);
 
  // ponto flutuante 
+ const numeroPontoFlutuante = 3.3;
+ const numeroPontoFlutuanteSemZero = .5
